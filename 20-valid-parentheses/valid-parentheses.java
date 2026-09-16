ea02a64868507d71f78d0e0bba6ff1a20a7997cf
Shivam Kumar <shiviam02@gmail.com>
2026-09-16 18:05:50 +0530

    Time: 3 ms (86.03%) | Memory: 43.4 MB (40.90%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        char arr[]=s.toCharArray();
+        for(char c:arr){
+            if(c=='{'||c=='['||c=='('){
+                if(c=='{') stack.push('}');
+                else if(c=='[') stack.push(']');
+                else stack.push(')');
+            }
+            else{
+                if(stack.isEmpty() || (!stack.isEmpty() && c!=stack.pop())) return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
